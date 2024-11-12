74ce4f3e0d1ecdb1096f0b1dcde9dc8a04c7cb3b
joaquin315 <157328119+joaquin315@users.noreply.github.com>
2024-11-12 19:30:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,97 @@
-import streamlit as st
+import streamlit as st #importar la libreria
 from groq import Groq
 
-st.set_page_config(page_title="Mi chat de IA", page_icon="😎")
-nombre = ""
-st.title("¡Bienvenido! Un gusto verlo.")
-nombre = st.text_input("¿Cuál es su nombre?")
-if st.button("Saludito") :
-    st.write(f"Hola, {nombre}!, Espero que estes bien :)")
+#Le damos un titulo a la pestaña de la web
+st.set_page_config(page_title="Mi chat de IA", page_icon="🎮") 
+
+#Titulo de la pagina
+st.title("Mi primera aplacación con Streamlit")
+
+#Ingreso de datos
+nombre = st.text_input("¿Cuál es tu nombre?")
+
+#Crear un botón con funcionalidad
+if st.button("Saludar") :
+    st.write(f"¡Hola, {nombre}! Gracias por venir a Talento Tech")
 
 MODELO = ['llama3-8b-8192', 'llama3-70b-8192', 'mixtral-8x7b-32768']
 
-#Conectar a la API
+#Nos conectamos a API
 def crear_usuario_groq():
-    clave_secreta = st.secrets["CLAVE_API"] #Toma la clave
-    return Groq(api_key = clave_secreta) #Crear el usuario
+    clave_secreta = st.secrets["CLAVE_API"] #Obtiene la clave de la API
+    return Groq(api_key = clave_secreta) #Conectamos con la API
 
-def configurar_modelo(cliente, modelo, mensajeEntrente):
+#Configuramos el Modelo que se va a usar
+def configurar_modelo(cliente, modelo, mensajeDeEntrada):
     return cliente.chat.completions.create(
-        model = modelo,
-        messages = [{"role":"user","content": mensajeEntrente}], #Quin manda el mensaje
-        stream = True
+        model = modelo, #Indica el modelo de la IA
+        messages = [{"role": "user", "content": mensajeDeEntrada}],
+        stream = True #Para que el modelo responda a tiempo
     )
 
-def inicializar_estado():# --> Historial de mensajes
-    # si mansajes no esta en st.session_state: el usuario nunca ingreso un mensaje
+#Historial de mensaje
+def inicializar_estado():
+    #Si no existe una lista llamada "mensajes" -> creamos uno
     if "mensajes" not in st.session_state:
-        st.session_state.mensajes = [] #Memoria de mensajes
+        st.session_state.mensajes = [] #Lista vacia (Historial vacio)
 
 def actualizar_historial(rol, contenido, avatar):
-     st.session_state.mensajes.append({"role":rol, "content":contenido, "avatar": avatar})
+    #El metodo apped() agrega un elemento a la lista
+    st.session_state.mensajes.append(
+        {"role": rol, "content": contenido, "avatar" : avatar}
+    )
 
 def mostrar_historial():
     for mensaje in st.session_state.mensajes:
-        # with crea el apartado del mansaje, hace un bloque agrupando el avatar, el usuario y el mansaje
-        with st.chat_message(mensaje["role"], avatar = mensaje["avatar"]) : st.markdown(mensaje["content"])
+        with st.chat_message(mensaje["role"], avatar= mensaje["avatar"]) : 
+            st.markdown(mensaje["content"])
 
-# Contenedor del chat
+#Contenedor del chat
 def area_chat():
-    contenedorDelChat = st.container(height=400, border= True)
-    # Agrupamos los mensajes en el area del chta
-    with contenedorDelChat: mostrar_historial()
+    contenedorDelChat = st.container(height= 400, border= True)
+    #Agrupamos los mensajes en el area del chat
+    with contenedorDelChat : mostrar_historial()
 
-def generar_respuesta(chat_completo):
-    respuesta_completa = ""
+#? CREANDO FUNCIÓN -> con diseño de la pagina
+def configurar_pagina():
+    st.title("Mi chat de IA")
+    st.sidebar.title("Configuración")
+    seleccion = st.sidebar.selectbox(
+        "Elegí un modelo", #Titulo
+        MODELO, #Tiene que estar en una lista
+        index = 0 #datoDefecto
+    )
+
+    return seleccion #Devuelve un dato
+
+def generar_respuestas(chat_completo):
+    respuesta_completa = "" #Texto vacio
     for frase in chat_completo:
         if frase.choices[0].delta.content:
             respuesta_completa += frase.choices[0].delta.content
             yield frase.choices[0].delta.content
-    return respuesta_completa
 
-# Creando una función -> con diseño de la pagina
-def config_web():
-    st.title("Mi chat de IA re pro.")
-    st.sidebar.title("Configuración.")
-    # seleccion2 = st.selectbox
-    seleccion1 = st.sidebar.selectbox(
-        "Elegi un modelo",
-        MODELO,
-        index = 0
-    )
-    return seleccion1
+    return respuesta_completa 
 
-def main():
-    #-----------------------------INVOCACIÓN DE FUNCIONES-----------------------------
-    modelo = config_web()
-    clienteUsuario = crear_usuario_groq()
-    inicializar_estado()
-    area_chat() #Creamos el sector donde vemos los mensajes
-    mensaje = st.chat_input("Hola, ¿Cuánto vale la raíz cuadrada de 2?")
-    # Verificar si el mansaje tiene contenido:
+def main(): #Función principal
+    #INVOCACIÓN DE FUNCIONES
+    modelo = configurar_pagina() #llamamos a la función
+    clienteUsuario = crear_usuario_groq() #Crea el usuario para usar la API
+    inicializar_estado() #Crea el historial vacio de mensaje
+    area_chat() #? Creamos el sector para ver los mensajes
+    mensaje = st.chat_input("Escribí tu mensaje...")
+
+    #Verificamos si el mensaje tiene contenido
     if mensaje:
-        actualizar_historial("user", mensaje, "🦄")
+        actualizar_historial("user", mensaje, "👩‍🏫") #Visualizamos el msg del usuario
         chat_completo = configurar_modelo(clienteUsuario, modelo, mensaje)
         if chat_completo:
-            with st.chat_message("assistant"):
-                respuesta_completa = st.write_stream(generar_respuesta(chat_completo))
+            with st.chat_message("assistant") :
+                respuesta_completa = st.write_stream(generar_respuestas(chat_completo))
                 actualizar_historial("assistant", respuesta_completa, "🤖")
-                st.rerun()
-
-
-        # generar_respuesta(chat_completo)
-        # actualizar_historial("assistant", chat_completo, "🤖")
-        # st.rerun()
-
-
-    # st.write(f'El usuario elegio la opcion "{modelo}".')
-
-#Indicamos que nuestra funcion principal es main
-# if __name__ == "__name__":
-#     main()
+            st.rerun()
+                
+#Indicamos que nuestra función principal es main()
+if __name__ == "__main__":
+    main()
 main()
